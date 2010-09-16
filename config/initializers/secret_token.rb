82879c660e706d498970e8c5dda12ae7bc79fd32
Chris Blunt <chris@chrisblunt.com>
2010-09-16 09:29:44 +0100

    Installed new Rails 3 project.
      * No Prototype
      * PostgreSQL Database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3ProjectTemplate::Application.config.secret_token = '22fe19062dce59440c378023928aa9b26c8d2a2f99bf74f7425e6d058d653af1dea873aaa85a5124031f1de2c90955298e64e2391e003883936504f08bf52d9f'
